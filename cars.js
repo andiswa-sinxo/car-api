b7076ecd018030bc3e5d3e421560f5aad876d59c
andiswa-sinxo <andiswasinxo@gmail.com>
2022-04-01 12:11:53 +0200

    style the tables

--- cars.js
+++ cars.js
@@ -1,16 +1,73 @@
 const carElem = document.querySelector(".cars");
+const colorElem = document.querySelector(".colors")
+const brandElem = document.querySelector(".brands")
+const filter = document.querySelector(".filterCars")
+const brandSelec = document.querySelector(".brandSelect")
+const colorSelec = document.querySelector(".colorSelect")
+const btn = document.querySelector(".btnFilter")
+const display = document.querySelector(".display")
 
+const templateFilter = Handlebars.compile(filter.innerHTML)
+var filterTheCars = []
+
+btn.addEventListener("click", function(){
+        var colors = colorSelec.value
+        var makes = brandSelec.value
+
+        axios
+        .get("https://api-tutor.herokuapp.com/v1/cars/make/" + makes + '/color/' + colors)
+        .then(function (results){
+            
+            let data = results.data;
+            filterTheCars.push(data)
+
+
+            console.log(filterTheCars);
+            display.innerHTML = templateFilter({
+                filterTheCars : data
+            })
+        })
+
+})
+
+axios
+    .get("https://api-tutor.herokuapp.com/v1/colors")
+    .then(function (results) {
+        results.data.forEach(color => {
+
+            const li = document.createElement("ul");
+            li.innerHTML = `<ul>
+            <li>${color}</li>
+        </ul>`
+            colorElem.appendChild(li)
+        });
+    })
+
+    axios
+    .get("https://api-tutor.herokuapp.com/v1/makes")
+    .then(function (results) {
+        results.data.forEach(makes => {
+
+            const li = document.createElement("ul");
+            li.innerHTML = `<ul>
+            <li>${makes}</li>
+        </ul>`
+            brandElem.appendChild(li)
+        });
+    })
 
 axios
     .get("https://api-tutor.herokuapp.com/v1/cars")
-    .then(function (resutls) {
-        resutls.data.forEach(car => {
+    .then(function (results) {
+        results.data.forEach(car => {
 
             const li = document.createElement("tr");
             li.innerHTML = `<tr>
             <td>${car.make}</td>
             <td>${car.model}</td>
             <td>${car.color}</td>
+            <td>${car.price}</td>
+            <td>${car.reg_number}</td>
         </tr>`
             carElem.appendChild(li)
         });
